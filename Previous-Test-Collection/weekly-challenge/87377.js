a830225017b2c562490af57193896a1b363dbd56
KyeongHa <rudgk030@hanyang.ac.kr>
2023-04-12 17:22:26 +0900

    2023-04-12

--- Previous-Test-Collection/weekly-challenge/87377.js
+++ Previous-Test-Collection/weekly-challenge/87377.js
@@ -1,5 +1,32 @@
-// 교점에 별 만들기
 function solution(line) {
-    var answer = [];
-    return answer;
+    const result = [];
+    let [maxH, minH, maxW, minW] = [-Infinity, Infinity, -Infinity, Infinity];
+    
+    line.forEach(([a, b, e], i) => {
+        for (let j = i + 1; j < line.length; j++) {
+            const [c, d, f] = line[j];
+            const mod = a * d - b * c;
+            
+            if (mod === 0) continue; // 분모가 0인 경우 -> 서로 평행하거나 일치
+
+            const xNum = b * f - e * d;
+            const yNum = e * c - a * f;
+            if (xNum % mod || yNum % mod) continue; // 정수가 아닌 교차점
+
+            const [x, y] = [xNum / mod, yNum / mod];
+            result.push([x, y]); // 교차점 추가
+    
+            maxW = Math.max(x, maxW);
+            minW = Math.min(x, minW);
+            maxH = Math.max(y, maxH);
+            minH = Math.min(y, minH);
+        }
+    });
+    const table = new Array(maxH - minH + 1)
+    .fill(null)
+    .map(() => new Array(maxW - minW + 1).fill(null).map(() => "."));
+
+    result.forEach(([x, y]) => (table[maxH - y][x - minW] = "*"));
+
+    return table.map((v) => v.join(""));
 }
